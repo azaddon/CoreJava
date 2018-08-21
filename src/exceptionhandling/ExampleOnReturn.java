87d19b465a6f758d132abe8cfd253f0c29cfd223
mohdazad.khan <mohdazad.khan@INL-7030YC2.groupinfra.com>
2018-08-21 18:26:29 +0530

    Return statement code check

---
+++ src/exceptionhandling/ExampleOnReturn.java
@@ -0,0 +1,28 @@
+package exceptionhandling;
+
+public class ExampleOnReturn {
+
+	public int doMethod() {
+		try {
+			int sum =10/0;
+		}
+		catch(ArithmeticException ae) {
+			System.out.println("exception caught");
+			return 4;
+		}
+		catch(Exception e) {
+			System.out.println("this is of no use");
+		}
+		finally {
+			return 10;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExampleOnReturn eor = new ExampleOnReturn();
+		int rval = eor.doMethod();
+		System.out.println("THe return value will be" + rval);
+	}
+
+}
